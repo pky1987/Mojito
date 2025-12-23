8bc699ef6b9a06fa04b9fbeedaebc1fba9f7519a
pky1987 <py430896@gmail.com>
2025-12-23 15:53:05 +0530

    Implement About Section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import gsap from 'gsap'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Cocktails from './components/Cocktails'
+import About from './components/About'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
     </main>
   )
 }
